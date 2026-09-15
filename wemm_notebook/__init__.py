3f003abf3755a31c610b2ea318d844e26fc769d3
Dang Khoa <i.am@dangkhoa.dev>
2026-09-15 19:53:52 +0700

    refactor(notebook): export sectioned phase API

--- wemm_notebook/__init__.py
+++ wemm_notebook/__init__.py
@@ -1,5 +1,21 @@
 """Readable orchestration layer for the public Kaggle notebook."""
 
-from .runner import run_public_notebook
+from .runner import (
+    abort_public_session,
+    run_public_notebook,
+    run_step6,
+    run_step7a,
+    run_step7b,
+    run_step8,
+    start_public_session,
+)
 
-__all__ = ["run_public_notebook"]
+__all__ = [
+    "abort_public_session",
+    "run_public_notebook",
+    "run_step6",
+    "run_step7a",
+    "run_step7b",
+    "run_step8",
+    "start_public_session",
+]
